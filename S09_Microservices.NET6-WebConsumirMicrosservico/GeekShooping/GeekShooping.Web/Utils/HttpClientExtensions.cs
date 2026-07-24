a048108674bc55e43fb47225ba7670234582524f
Victor R Santos <vrsisa@hotmail.com>
2026-07-23 21:36:26 -0300

    Adição de HttpClientExtensions com o método ReadContentAsync<T>
    Adição da classe estática HttpClientExtensions em GeekShopping.Web.Utils.
    Implementação da extensão ReadContentAsync<T> para HttpResponseMessage, visando desserializar respostas JSON ignorando a diferenciação entre maiúsculas e minúsculas nos nomes das propriedades. Lança uma ApplicationException em caso de respostas malsucedidas.

---
+++ S09_Microservices.NET6-WebConsumirMicrosservico/GeekShooping/GeekShooping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAsync<T>(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            else
+            {
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
+        }
+    }
+}
