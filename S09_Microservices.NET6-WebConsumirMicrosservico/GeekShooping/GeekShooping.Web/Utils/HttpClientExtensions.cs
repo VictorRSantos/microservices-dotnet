1e0be34a56f32579516bdf58819496039d78082c
Victor R Santos <vrsisa@hotmail.com>
2026-07-23 21:46:00 -0300

    Implementacao da extensao PostAsJson e PutAsJson

--- S09_Microservices.NET6-WebConsumirMicrosservico/GeekShooping/GeekShooping.Web/Utils/HttpClientExtensions.cs
+++ S09_Microservices.NET6-WebConsumirMicrosservico/GeekShooping/GeekShooping.Web/Utils/HttpClientExtensions.cs
@@ -1,9 +1,13 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace GeekShopping.Web.Utils
 {
     public static class HttpClientExtensions
     {
+        private static MediaTypeHeaderValue _contentType = new MediaTypeHeaderValue("application/json");
+
         public static async Task<T> ReadContentAsync<T>(this HttpResponseMessage response)
         {
             if (response.IsSuccessStatusCode)
@@ -15,6 +19,28 @@ namespace GeekShopping.Web.Utils
             {
                 throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
             }
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(
+                dataAsString,
+                Encoding.UTF8,
+                _contentType.MediaType);
+
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(
+                dataAsString,
+                Encoding.UTF8,
+                _contentType.MediaType);
+
+            return httpClient.PutAsync(url, content);
         }
     }
 }
